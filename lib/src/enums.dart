a4fa50089bf7de38b932a0a8e090e74cd3f3c007
Eugene <eugenemrlens@gmail.com>
2022-01-31 12:18:53 +0300

    chore: add flavor enums

--- lib/src/enums.dart
+++ lib/src/enums.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/foundation.dart';
+
 enum Country { kenya, uganda, tanzania, us, uk, belgium, nigeria }
+
+/// [Flavour]
+enum Flavour { consumer, pro, unknown }
+
+/// [FlavourEx] extends [Flavour] enum to get the values as
+/// strings
+extension FlavourEx on Flavour {
+  String get name {
+    return describeEnum(this).toUpperCase();
+  }
+}
