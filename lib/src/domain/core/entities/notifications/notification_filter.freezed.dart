ee0d9a324e8a7154aecba7e96fab2fdbcf6effd1
Byron Kimani <byronkimani@Byrons-MacBook-Pro.local>
2022-07-21 15:25:53 +0300

    feat: add notification filters
    - add notification filters
    
    Signed-off-by: Byron Kimani <byronkimani@Byrons-MacBook-Pro.local>

---
+++ lib/src/domain/core/entities/notifications/notification_filter.freezed.dart
@@ -0,0 +1,188 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'notification_filter.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+NotificationFilter _$NotificationFilterFromJson(Map<String, dynamic> json) {
+  return _NotificationFilter.fromJson(json);
+}
+
+/// @nodoc
+mixin _$NotificationFilter {
+  @JsonKey(name: 'name')
+  String? get name => throw _privateConstructorUsedError;
+  @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+  NotificationType? get type => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $NotificationFilterCopyWith<NotificationFilter> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $NotificationFilterCopyWith<$Res> {
+  factory $NotificationFilterCopyWith(
+          NotificationFilter value, $Res Function(NotificationFilter) then) =
+      _$NotificationFilterCopyWithImpl<$Res>;
+  $Res call(
+      {@JsonKey(name: 'name')
+          String? name,
+      @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+          NotificationType? type});
+}
+
+/// @nodoc
+class _$NotificationFilterCopyWithImpl<$Res>
+    implements $NotificationFilterCopyWith<$Res> {
+  _$NotificationFilterCopyWithImpl(this._value, this._then);
+
+  final NotificationFilter _value;
+  // ignore: unused_field
+  final $Res Function(NotificationFilter) _then;
+
+  @override
+  $Res call({
+    Object? name = freezed,
+    Object? type = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as NotificationType?,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$$_NotificationFilterCopyWith<$Res>
+    implements $NotificationFilterCopyWith<$Res> {
+  factory _$$_NotificationFilterCopyWith(_$_NotificationFilter value,
+          $Res Function(_$_NotificationFilter) then) =
+      __$$_NotificationFilterCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {@JsonKey(name: 'name')
+          String? name,
+      @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+          NotificationType? type});
+}
+
+/// @nodoc
+class __$$_NotificationFilterCopyWithImpl<$Res>
+    extends _$NotificationFilterCopyWithImpl<$Res>
+    implements _$$_NotificationFilterCopyWith<$Res> {
+  __$$_NotificationFilterCopyWithImpl(
+      _$_NotificationFilter _value, $Res Function(_$_NotificationFilter) _then)
+      : super(_value, (v) => _then(v as _$_NotificationFilter));
+
+  @override
+  _$_NotificationFilter get _value => super._value as _$_NotificationFilter;
+
+  @override
+  $Res call({
+    Object? name = freezed,
+    Object? type = freezed,
+  }) {
+    return _then(_$_NotificationFilter(
+      name: name == freezed
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String?,
+      type: type == freezed
+          ? _value.type
+          : type // ignore: cast_nullable_to_non_nullable
+              as NotificationType?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_NotificationFilter implements _NotificationFilter {
+  _$_NotificationFilter(
+      {@JsonKey(name: 'name')
+          this.name,
+      @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+          this.type});
+
+  factory _$_NotificationFilter.fromJson(Map<String, dynamic> json) =>
+      _$$_NotificationFilterFromJson(json);
+
+  @override
+  @JsonKey(name: 'name')
+  final String? name;
+  @override
+  @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+  final NotificationType? type;
+
+  @override
+  String toString() {
+    return 'NotificationFilter(name: $name, type: $type)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_NotificationFilter &&
+            const DeepCollectionEquality().equals(other.name, name) &&
+            const DeepCollectionEquality().equals(other.type, type));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      const DeepCollectionEquality().hash(name),
+      const DeepCollectionEquality().hash(type));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$_NotificationFilterCopyWith<_$_NotificationFilter> get copyWith =>
+      __$$_NotificationFilterCopyWithImpl<_$_NotificationFilter>(
+          this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_NotificationFilterToJson(this);
+  }
+}
+
+abstract class _NotificationFilter implements NotificationFilter {
+  factory _NotificationFilter(
+      {@JsonKey(name: 'name')
+          final String? name,
+      @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+          final NotificationType? type}) = _$_NotificationFilter;
+
+  factory _NotificationFilter.fromJson(Map<String, dynamic> json) =
+      _$_NotificationFilter.fromJson;
+
+  @override
+  @JsonKey(name: 'name')
+  String? get name => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(name: 'type', defaultValue: NotificationType.UNKNOWN)
+  NotificationType? get type => throw _privateConstructorUsedError;
+  @override
+  @JsonKey(ignore: true)
+  _$$_NotificationFilterCopyWith<_$_NotificationFilter> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/src/domain/core/entities/notifications/notification_filter.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notification_filter.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_NotificationFilter _$$_NotificationFilterFromJson(
+        Map<String, dynamic> json) =>
+    _$_NotificationFilter(
+      name: json['name'] as String?,
+      type: $enumDecodeNullable(_$NotificationTypeEnumMap, json['type']) ??
+          NotificationType.UNKNOWN,
+    );
+
+Map<String, dynamic> _$$_NotificationFilterToJson(
+        _$_NotificationFilter instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'type': _$NotificationTypeEnumMap[instance.type],
+    };
+
+const _$NotificationTypeEnumMap = {
+  NotificationType.APPOINTMENT: 'APPOINTMENT',
+  NotificationType.SERVICE_REQUEST: 'SERVICE_REQUEST',
+  NotificationType.COMMUNITIES: 'COMMUNITIES',
+  NotificationType.SURVEYS: 'SURVEYS',
+  NotificationType.ROLE_ASSIGNMENT: 'ROLE_ASSIGNMENT',
+  NotificationType.ROLE_REVOCATION: 'ROLE_REVOCATION',
+  NotificationType.DEMOTE_MODERATOR: 'DEMOTE_MODERATOR',
+  NotificationType.PROMOTE_TO_MODERATOR: 'PROMOTE_TO_MODERATOR',
+  NotificationType.UNKNOWN: 'UNKNOWN',
+};
